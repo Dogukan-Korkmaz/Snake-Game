401357b069b62c9b8821cc36687987cd951bcbec
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-01-31 22:31:11 +0300

    Added snake.py for movement.(created a class for all snake material)

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,28 +9,24 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-for positions in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(positions)
-    segments.append(new_segment)
-
+snake.create_snake()
 
 is_game_on = True
 
+
 while is_game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.05)
+    snake.move_snake()
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
 
 
 screen.exitonclick()
